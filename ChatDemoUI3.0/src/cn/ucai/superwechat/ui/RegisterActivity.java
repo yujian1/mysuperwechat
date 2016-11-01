028f1eb9af695587bb010ce5476b089af07a6c1a
刘宁 <1039212799@qq.com>
2016-11-01 17:52:23 +0800

    解决app_name的冲突问题

--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/RegisterActivity.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/RegisterActivity.java
@@ -13,18 +13,19 @@
  */
 package cn.ucai.superwechat.ui;
 
-import com.hyphenate.EMError;
-import com.hyphenate.chat.EMClient;
-import cn.ucai.superwechat.DemoHelper;
-import cn.ucai.superwechat.R;
-import com.hyphenate.exceptions.HyphenateException;
-
 import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.hyphenate.EMError;
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.exceptions.HyphenateException;
+
+import cn.ucai.superwechat.DemoHelper;
+import cn.ucai.superwechat.R;
 
 /**
  * register screen
@@ -41,7 +42,7 @@ public class RegisterActivity extends BaseActivity {
 		setContentView(R.layout.em_activity_register);
 		userNameEditText = (EditText) findViewById(R.id.username);
 		passwordEditText = (EditText) findViewById(R.id.password);
-		confirmPwdEditText = (EditText) findViewById(R.id.confirm_password);
+		confirmPwdEditText = (EditText) findViewById(R.id.confrim_password);
 	}
 
 	public void register(View view) {
--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/SplashActivity.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/SplashActivity.java
@@ -2,14 +2,12 @@ package cn.ucai.superwechat.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.animation.AlphaAnimation;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 import com.hyphenate.chat.EMClient;
+import com.hyphenate.util.EasyUtils;
+
 import cn.ucai.superwechat.DemoHelper;
 import cn.ucai.superwechat.R;
-import com.hyphenate.util.EasyUtils;
 
 /**
  * 开屏页
@@ -24,13 +22,7 @@ public class SplashActivity extends BaseActivity {
 		setContentView(R.layout.em_activity_splash);
 		super.onCreate(arg0);
 
-		RelativeLayout rootLayout = (RelativeLayout) findViewById(R.id.splash_root);
-		TextView versionText = (TextView) findViewById(R.id.tv_version);
 
-		versionText.setText(getVersion());
-		AlphaAnimation animation = new AlphaAnimation(0.3f, 1.0f);
-		animation.setDuration(1500);
-		rootLayout.startAnimation(animation);
 	}
 
 	@Override

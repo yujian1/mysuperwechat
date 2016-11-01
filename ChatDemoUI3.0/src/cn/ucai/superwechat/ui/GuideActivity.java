33ed02c78cede6fb784bbcc0d9baf8fe51d3f6d9
刘宁 <1039212799@qq.com>
2016-11-01 19:32:25 +0800

    添加butterknife和欢迎界面

--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/GuideActivity.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/GuideActivity.java
@@ -1,8 +1,12 @@
 package cn.ucai.superwechat.ui;
 
 import android.os.Bundle;
+import android.view.View;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import cn.ucai.superwechat.R;
+import cn.ucai.superwechat.utils.MFGT;
 
 public class GuideActivity extends BaseActivity {
 
@@ -10,5 +14,18 @@ public class GuideActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.btn_login, R.id.btn_register})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_login:
+                MFGT.gotologin(this);
+                break;
+            case R.id.btn_register:
+                MFGT.gotoRegister(this);
+                break;
+        }
     }
 }

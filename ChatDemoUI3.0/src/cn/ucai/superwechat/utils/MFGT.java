3bfc988263b12eebb1ba2021f449c08100b76d79
刘宁 <1039212799@qq.com>
2016-11-01 19:30:59 +0800

    添加跳转和log的工具类

---
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/utils/MFGT.java
@@ -0,0 +1,41 @@
+package cn.ucai.superwechat.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+
+import cn.ucai.superwechat.R;
+import cn.ucai.superwechat.ui.LoginActivity;
+import cn.ucai.superwechat.ui.RegisterActivity;
+
+
+public class MFGT {
+    public static void finish(Activity activity){
+        activity.finish();
+        activity.overridePendingTransition(R.anim.push_right_in, R.anim.push_right_out);
+    }
+
+    public static void startActivity(Activity context,Class<?> cls){
+        Intent intent = new Intent();
+        intent.setClass(context,cls);
+        startActivity(context,intent);
+    }
+
+    public static void startActivity(Context context, Intent intent){
+        context.startActivity(intent);
+        ((Activity)context).overridePendingTransition(R.anim.push_left_in,R.anim.push_left_out);
+
+    }
+
+    public static void startActivityForResult(Activity context,Intent intent,int requestCode){
+        context.startActivityForResult(intent,requestCode);
+        context.overridePendingTransition(R.anim.push_left_in,R.anim.push_left_out);
+    }
+    public static void gotologin(Activity context){
+        startActivity(context, LoginActivity.class);
+    }
+    public static void gotoRegister(Activity context){
+        startActivity(context, RegisterActivity.class);
+    }
+
+}

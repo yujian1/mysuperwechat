4256c5370db8905412ce167c5a56663075c49576
刘宁 <1039212799@qq.com>
2016-11-08 18:48:11 +0800

    新的朋友处理逻辑

--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/DiscoverFragment.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/DiscoverFragment.java
@@ -15,7 +15,7 @@ public class DiscoverFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_discover, container, false);
+        return inflater.inflate(R.layout.fragment_dicover, container, false);
     }
 
 //    //朋友圈点击事件
